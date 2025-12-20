73e031c0f65934b40c8a919050773fe7032dfac3
Nobi-123 <nobita.infringement.report@gmail.com>
2025-12-20 21:13:16 +0530

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -86,8 +86,7 @@ async def start_pm(client, message: Message, _):
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
-
-        try:
+        
             out = private_panel(_)
             await message.react("❤️")
     loading_1 = await message.reply_text("𝑯𝒐𝒊𝒊!! 𝑱𝒂𝒏𝒖𝒖𝒖𝒖𝒖 </🥀`❤️")
