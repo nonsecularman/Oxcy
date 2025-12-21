c40c5dea34c4d6825f5e933f1f3ce6de3f938ec7
Nobi-123 <nobita.infringement.report@gmail.com>
2025-12-21 10:13:09 +0530

    Fix typo in SUPPORT_CHAT URL

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/veron_bots")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/adult_towm")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/adult_town")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
